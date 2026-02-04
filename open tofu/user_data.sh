7ef02b4149277e6f8db67efe42fb23e44c4c4793
Rémi Pépin <pepin.remi@gmail.com>
2026-02-04 09:38:07 +0100

    up

--- open tofu/user_data.sh
+++ open tofu/user_data.sh
@@ -8,8 +8,8 @@ cd projet/webservice
 python3 -m venv venv
 source venv/bin/activate
 rm .env
-echo 'BUCKET=${git_repo}' >> .env
-echo 'DYNAMO_TABLE=${git_repo}' >> .env
+echo 'BUCKET=${bucket}' >> .env
+echo 'DYNAMO_TABLE=${dynamo_table}' >> .env
 pip3 install -r requirements.txt
 venv/bin/python app.py
 echo "userdata-end
